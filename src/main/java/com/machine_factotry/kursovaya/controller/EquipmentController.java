21cdd72694cc67a717f7a9f30ccdd6639c36ce19
deloire <sashamax098760@gmail.com>
2025-05-02 14:46:51 +0300

    now code returns data from db which contains in db

--- src/main/java/com/machine_factotry/kursovaya/controller/EquipmentController.java
+++ src/main/java/com/machine_factotry/kursovaya/controller/EquipmentController.java
@@ -1,13 +1,24 @@
 package com.machine_factotry.kursovaya.controller;
 
+import com.machine_factotry.kursovaya.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class EquipmentController {
 
+    @Autowired
+    EquipmentService equipmentService;
+
     @GetMapping("/equipment")
-    public String equipment() {
+    public String equipment(Model model) {
+        List<Map<String, Object>> equipments = equipmentService.getEquipmentData();
+        model.addAttribute("equipment", equipments);
         return "equipment";
     }
 }
